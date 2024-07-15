9de2b835eabe47244aff1d202a780b31885b962a
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-15 09:43:32 +0300

    test: email validation

--- tests/CodeForcer.Tests/Features/Students/CreateStudentTests.cs
+++ tests/CodeForcer.Tests/Features/Students/CreateStudentTests.cs
@@ -1,4 +1,6 @@
 using CodeForcer.Contracts;
+using CodeForcer.Features.Students.Common;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CodeForcer.Tests.Features.Students;
 
@@ -12,17 +14,35 @@ public class CreateStudentTests(IntegrationTestWebAppFactory factory)
         var student = Fakers.StudentsFaker.Generate();
 
         var request = new CreateStudentRequest(student.Email!, student.Handle);
-        
+
         //Act
         var response = await Client.PostAsJsonAsync("/students", request);
-        
+
         //Assert
         response.StatusCode.Should().Be(HttpStatusCode.Created);
-        
+
         var responseStudent = await response.Content.ReadFromJsonAsync<StudentResponse>();
         responseStudent.Should().BeEquivalentTo(student);
 
         var dbStudent = await StudentsRepository.GetByEmail(student.Email!);
         dbStudent.Should().BeEquivalentTo(student);
     }
+
+    [Fact]
+    public async Task ShouldReturnBadRequestWithProblemDetails_WhenEmailIsInvalid()
+    {
+        //Arrange
+        var student = Fakers.StudentsFaker.Generate();
+        var request = new CreateStudentRequest("invalid-email", student.Handle);
+
+        //Act
+        var response = await Client.PostAsJsonAsync("/students", request);
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+        problemDetails?.Title.Should().Be(StudentsErrors.InvalidEmail.Description);
+    }
+
+    //TODO add handle validation
 }
